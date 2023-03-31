534dc1b1c0f1a17d4f49135f77e73d1c7b17af8f
Helena Luz <99624336+helenaluz@users.noreply.github.com>
2023-03-31 18:29:28 -0300

    Update Uni3URI1050.java

--- Uni3URI1050.java
+++ Uni3URI1050.java
@@ -20,15 +20,16 @@ public class Uni3URI1050{
 		
 		switch(DDD) {
 		case 61: System.out.println("Brasilia");break;
-		case 71: System.out.println("Salavador");break;
+		case 71: System.out.println("Salvador");break;
 		case 11: System.out.println("Sao Paulo");break;
 		case 21: System.out.println("Rio de Janeiro");break;
 		case 32: System.out.println("Juiz de Fora");break;
 		case 19: System.out.println("Campinas");break;
 		case 27: System.out.println("Vitoria");break;
 		case 31: System.out.println("Belo Horizonte");break;
-		default: System.out.println("DDD nao cadastrado");
-		}
+		default: System.out.println("DDD nao cadastrado");break;}
+		
+		scanner.close();
 	}
 }
 
